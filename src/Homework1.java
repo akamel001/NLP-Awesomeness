c21ccc81a12905a631fbde347a9700a1a0b0a5c1
Jonathan Pullano <jp938@cornell.edu>
2012-02-17 16:08:36 -0500

    Does probabilities :)

--- src/Homework1.java
+++ src/Homework1.java
@@ -9,14 +9,16 @@ import java.util.Scanner;
 public class Homework1 {
     
     private static HashMap<String, Integer> ngrams = new HashMap<String, Integer>();
-    //private static HashMap<String, Integer> nminusonegrams = new HashMap<String, Integer>();
+    private static HashMap<String, Integer> nMinusOneGrams = new HashMap<String, Integer>();
     
     /**
      * @param args
      */
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+        findNGrams("omg", 5);
+        System.out.println(ngrams);
+        System.out.println(nMinusOneGrams);
+        System.out.println(probability("my cat is on fire"));
     }
     
     /**
@@ -30,14 +32,17 @@ public class Homework1 {
             while (scanner.hasNextLine()) {
                 plaintext += scanner.nextLine() + "\n";
             }
-            ArrayList<String> words = new ArrayList<String>(Arrays.asList(plaintext.split(" \t")));
+            ArrayList<String> words = new ArrayList<String>(Arrays.asList(plaintext.split(" |\n")));
             while(words.remove("")); //Eliminate multiple consecutive spaces
             for(int i = 0; i < words.size() - (n - 1); i++) {
-                String ngram = "";
-                for(int j = i; j < i + n; j++)
-                    ngram += words.get(j);
-                int ngramCount = ngrams.get(i) == null ? 0 : ngrams.get(i);
+                String ngram = getNGram(words, i, n);
+                int ngramCount = ngrams.get(ngram) == null ? 0 : ngrams.get(ngram);
                 ngrams.put(ngram, ngramCount + 1);
+                
+                String nMinusOneGram = getNGram(words, i, n - 1);
+                //System.out.println(nMinusOneGram);
+                int nMinusOneGramCount = nMinusOneGrams.get(nMinusOneGram) == null ? 0 : nMinusOneGrams.get(nMinusOneGram);
+                nMinusOneGrams.put(nMinusOneGram, nMinusOneGramCount + 1);
             }
         } catch (FileNotFoundException e) {
             System.out.println("File not found:" + filename);
@@ -46,6 +51,24 @@ public class Homework1 {
         }
     }
     
-    public static void probabilityTable() {}
-
+    public static String getNGram(ArrayList<String> words, int pos, int n) {
+        String ngram = "";
+        for(int j = pos; j < pos + n; j++)
+            if(j == pos + n - 1)
+                ngram += words.get(j);
+            else
+                ngram += words.get(j) + " ";
+        return ngram;
+    }
+    
+    public static double probability(String ngram) {
+        double numerator = ngrams.get(ngram) == null ? 0 : ngrams.get(ngram);
+        System.out.println(numerator);
+        String nMinusOneGram = ngram.substring(0, ngram.lastIndexOf(' '));
+        System.out.println(ngram.lastIndexOf(' '));
+        System.out.println(nMinusOneGram);
+        double denominator = nMinusOneGrams.get(nMinusOneGram) == null ? 0 : nMinusOneGrams.get(nMinusOneGram);
+        System.out.println(denominator);
+        return numerator / denominator;
+    }
 }
