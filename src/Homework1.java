0535d5d3096c052bb6d8f85454b0f7691397b616
Jonathan Pullano <jp938@cornell.edu>
2012-02-24 19:15:14 -0500

    Changed to sentence processing; Added beginning of sentence tags; bug fixes

--- src/Homework1.java
+++ src/Homework1.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map.Entry;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 
 public class Homework1 {
@@ -18,25 +19,54 @@ public class Homework1 {
 
 
     public static void main(String[] args) {
-        //bigram
         //TODO Does not work when n = 1 need
-        Homework1.findNGrams("data/test", 2);
-        //TODO Only workds for bigram not N-gram (fix later)
+        Homework1.findNGrams("data/EnronDataset/train.txt", 3);
 
+        buildProbabilityTable();
+
+        System.out.println(ngrams);
+        System.out.println(nMinusOneGrams);
+        System.out.println(bigram);
+    }
+
+    public static void buildProbabilityTable() {
+        System.out.println("Constructing Probability Tables");
+
+        //TODO Only workds for bigram not N-gram (fix later)
         Iterator<Entry<String, Integer>> itr1 = nMinusOneGrams.entrySet().iterator();
         Iterator<Entry<String, Integer>> itr2 = nMinusOneGrams.entrySet().iterator();
-        while(itr1.hasNext()){
+        while(itr1.hasNext()) {
             Entry<String, Integer> key1 = itr1.next();
-            while(itr2.hasNext()){
+            while(itr2.hasNext()) {
                 Entry<String, Integer> key2 = itr2.next();
-                String grams = key1.getKey() + " "+ key2.getKey();
-                double prob= probability(grams);
+                String grams = key1.getKey() + " " + key2.getKey();
+                double prob = probability(grams);
                 bigram.put(grams, prob);
             }
             itr2 = nMinusOneGrams.entrySet().iterator();
         }
+    }
 
-        System.out.println(bigram);
+    /**
+     * Reads a file, and returns sentences in an ArrayList.
+     * @param filename
+     * @param n
+     */
+    public static ArrayList<String> getSentences(String filename) {
+        System.out.println("Reading File:" + filename);
+        Scanner scanner = null;
+        ArrayList<String> sentences = new ArrayList<String>();
+        try {
+            scanner = new Scanner(new FileInputStream(filename));
+            scanner.useDelimiter(Pattern.compile("[.][^12345890]")); //End line with a period NOT FOLLOWED by a number (i.e. don't match 12.5)
+            while (scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine() + ".");
+                sentences.add(scanner.nextLine() + ".");
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found:" + filename);
+        }
+        return sentences;
     }
 
     /**
@@ -45,52 +75,60 @@ public class Homework1 {
      * @param n - Order of the language model
      */
     public static void findNGrams(String filename, int n) {
-        Scanner scanner = null;
-        try {
-            scanner = new Scanner(new FileInputStream(filename));
-            String plaintext = "";
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine() + "\n";
-                plaintext += line;
-            }
-            ArrayList<String> words = getWords(plaintext);
+        ArrayList<String> sentences = getSentences(filename);
+        ArrayList<String> words = getWords(sentences, n);
 
-            while(words.remove("")); //Eliminate multiple consecutive spaces
+        System.out.println("Removing Spaces");
 
-            for(int i = 0; (n != 1) && i < words.size() - (n - 1); i++) {
-                String ngram = getNGram(words, i, n);
-                int ngramCount = ngrams.get(ngram) == null ? 0 : ngrams.get(ngram);
-                ngrams.put(ngram, ngramCount + 1);
-            }
+        while(words.remove("")); //Eliminate multiple consecutive spaces
 
-            for(int i = 0; i <= words.size() - (n -1); i++){
-                String nMinusOneGram = getNGram(words, i, n -1);
-                int nMinusOneGramCount = nMinusOneGrams.get(nMinusOneGram) == null ? 0 : nMinusOneGrams.get(nMinusOneGram);
-                nMinusOneGrams.put(nMinusOneGram, nMinusOneGramCount + 1);
-            }
+        System.out.println("Building word frequency tables");
 
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found:" + filename);
-        } finally {
-            System.out.println(ngrams + "\n");
-            System.out.println(nMinusOneGrams + "\n");
-            scanner.close();
+        for(int i = 0; (n != 1) && i < words.size() - (n - 1); i++) {
+            String ngram = getNGram(words, i, n);
+            int ngramCount = ngrams.get(ngram) == null ? 0 : ngrams.get(ngram);
+            ngrams.put(ngram, ngramCount + 1);
+        }
+
+        for(int i = 0; i <= words.size() - (n - 1); i++){
+            String nMinusOneGram = getNGram(words, i, n - 1);
+            int nMinusOneGramCount = nMinusOneGrams.get(nMinusOneGram) == null ? 0 : nMinusOneGrams.get(nMinusOneGram);
+            nMinusOneGrams.put(nMinusOneGram, nMinusOneGramCount + 1);
         }
     }
 
     /**
-     * Given a string, returns a list of words contained in it
+     * Given a list of sentences, break it into a list of words.
      * @param string
+     * @param order of the model
      * @return
      */
-    public static ArrayList<String> getWords(String line) {
-        line = line.replace("." , " . ");
-        line = line.replace("," , " , ");
-        line = line.replace("\"", " \" ");
-        line = line.replace("\"", " \" ");
-        line = line.replace("(" , " ( ");
-        line = line.replace(")", " ) ");
-        return new ArrayList<String>(Arrays.asList(line.split(" |\n")));
+    public static ArrayList<String> getWords(ArrayList<String> sentences, int n) {
+        String fulltext = "";
+        System.out.println("Breaking sentences into words");
+        for(String sentence : sentences) {
+            fulltext += cleanSentence(sentence,n);
+        }
+
+        System.out.println("Building huge-ass array");
+        return new ArrayList<String>(Arrays.asList(fulltext.split(" |\n")));
+    }
+
+    public static String cleanSentence(String sentence, int n) {
+        System.out.println("cleanSentence called");
+        String prefix = "";
+        for(int i = 0; i < n - 1; i++) {
+            prefix += "<B> ";
+        }
+        //TODO: string replaces are a bit slow
+        sentence = prefix + sentence;
+        //sentence = sentence.replace("." , " .");
+        sentence = sentence.replace("," , " , ");
+        sentence = sentence.replace("\"", " \" ");
+        sentence = sentence.replace("\"", " \" ");
+        sentence = sentence.replace("(" , " ( ");
+        sentence = sentence.replace(")", " ) ");
+        return sentence;
     }
 
     /**
@@ -131,10 +169,10 @@ public class Homework1 {
      * @param probabilityTable - A particular language model to compute strings from
      * @param n - order of the language model (i.e. unigrams, bigrams, etc)
      */
-    public static double probabilityOfString(String string, HashMap<String, Double> probabilityTable, int n) {
+    public static double probabilityOfDocument(ArrayList<String> sentences, HashMap<String, Double> probabilityTable, int n) {
         //TODO: Look up probability of first n words
         //TODO: Consider log probabilities to avoid underflow
-        ArrayList<String> words = getWords(string);
+        ArrayList<String> words = getWords(sentences, n);
         double probability = 1.0;
         for(int i = 0; i < words.size() - n; i++) {
             probability *= probabilityTable.get(getNGram(words, i, n));
@@ -143,13 +181,13 @@ public class Homework1 {
     }
 
     /**
-     * Finds the perplexity of a string given a language model
+     * Finds the perplexity of a document given a language model
      * @param string
      * @param probabilityTable
      * @param n
      * @return
      */
-    public static double perplexity(String string, HashMap<String, Double> probabilityTable, int n) {
-        return Math.pow(1 / probabilityOfString(string, probabilityTable, n), 1.0 / n);
+    public static double perplexity(ArrayList<String> sentences, HashMap<String, Double> probabilityTable, int n) {
+        return Math.pow(1 / probabilityOfDocument(sentences, probabilityTable, n), 1.0 / n);
     }
 }
